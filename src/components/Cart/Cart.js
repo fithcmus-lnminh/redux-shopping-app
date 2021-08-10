dca42d8d7568f9ed3dfcd6b79d89e59041b72984
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-10 23:21:06 +0700

    adding add and remove handler

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,15 +1,27 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 const Cart = (props) => {
+  const products = useSelector((state) => state.cart.items);
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
       <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            item={{
+              title: product.name,
+              quantity: product.quantity,
+              total: product.totalPrice,
+              price: product.price,
+              id: product.id,
+            }}
+          />
+        ))}
       </ul>
     </Card>
   );
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,7 +1,24 @@
 import classes from "./CartItem.module.css";
+import { cartActions } from "../../store/cart-slice";
+import { useDispatch } from "react-redux";
 
 const CartItem = (props) => {
-  const { title, quantity, total, price } = props.item;
+  const { title, quantity, total, price, id } = props.item;
+  const dispatch = useDispatch();
+
+  const addToCartHandler = () => {
+    dispatch(
+      cartActions.addItemToCart({
+        id,
+        title,
+        price,
+      })
+    );
+  };
+
+  const removeItemHandler = () => {
+    dispatch(cartActions.removeItemFromCart(id));
+  };
 
   return (
     <li className={classes.item}>
@@ -17,8 +34,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={addToCartHandler}>+</button>
+          <button onClick={removeItemHandler}>-</button>
         </div>
       </div>
     </li>
