649dda81dfa392856b6da453a290916226284d5f
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-10 16:47:20 +0700

    setup store

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,4 +1,4 @@
-import classes from './CartItem.module.css';
+import classes from "./CartItem.module.css";
 
 const CartItem = (props) => {
   const { title, quantity, total, price } = props.item;
@@ -8,8 +8,8 @@ const CartItem = (props) => {
       <header>
         <h3>{title}</h3>
         <div className={classes.price}>
-          ${total.toFixed(2)}{' '}
-          <span className={classes.itemprice}>(${price.toFixed(2)}/item)</span>
+          ${total.toFixed(2)}{" "}
+          <span className={classes.itemPrice}>(${price.toFixed(2)}/item)</span>
         </div>
       </header>
       <div className={classes.details}>
