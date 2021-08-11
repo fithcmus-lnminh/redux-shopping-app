0c0f087cd4c8441820b0f746b3820c0bac00271d
Le Nhat Minh <lenhatminh11a1@gmail.com>
2021-08-11 22:14:48 +0700

    useEffect to send cart data to firebase

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const isVisible = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-b2a24-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT", //POST add new items as a list, PUT override the old data
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
